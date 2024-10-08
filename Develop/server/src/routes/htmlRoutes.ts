654101623f0cf4b03914fcfe36bdfa5ec8ce180c
vluu11 <luuvuong11@gmail.com>
2024-10-07 20:43:41 -0700

    fixed fetchWeather

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -6,8 +6,8 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+router.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
-  
+
 export default router;
--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'node:path';
+
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json()); 
